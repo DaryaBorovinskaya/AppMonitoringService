0b0512a155a7c1ef1a229789aaa45dd886fcf64a
DaryaBorovinskaya <dashhaha17@gmail.com>
2025-07-29 21:54:03 +0700

    Бэкап разные файлы

--- AppMonitoringService.UI/src/app/components/device-list/device-list.ts
+++ AppMonitoringService.UI/src/app/components/device-list/device-list.ts
@@ -1,9 +1,7 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { DeviceService } from '../../services/device.service';
 import { Device } from '../../models/device';
 import { Router } from '@angular/router';
-import { interval, Subscription } from 'rxjs';
-import { switchMap, startWith } from 'rxjs/operators';
 
 
 @Component({
@@ -12,10 +10,9 @@ import { switchMap, startWith } from 'rxjs/operators';
   templateUrl: './device-list.html',
   styleUrls: ['./device-list.less']
 })
-export class DeviceListComponent implements OnInit, OnDestroy {
+export class DeviceListComponent implements OnInit {
   devices: Device[] = [];
   displayedColumns: string[] = ['id', 'name', 'version', 'detail'];
-  private refreshSubscription!: Subscription;
 
   constructor(
     private deviceService: DeviceService,
@@ -24,30 +21,7 @@ export class DeviceListComponent implements OnInit, OnDestroy {
 
   ngOnInit(): void {
     this.loadDevices();
-    //this.setupAutoRefresh();
   }
-
-  ngOnDestroy(): void {
-    // отписаться при уничтожении компонента
-    if (this.refreshSubscription) {
-      this.refreshSubscription.unsubscribe();
-    }
-  }
-
-  //#region Метод периодического обновления (каждые 5 сек) для подгрузки новых данных 
-  // (в случае, если данные приходят очень часто)
-  // private setupAutoRefresh(): void {
-  //   this.refreshSubscription = interval(5000) // 5 секунд
-  //     .pipe(
-  //       startWith(0), // Запустить сразу при инициализации
-  //       switchMap(() => this.deviceService.getAllDevices())
-  //     )
-  //     .subscribe(
-  //       (data) => this.devices = data,
-  //       (error) => console.error('Error loading devices', error)
-  //     );
-  // }
-  //#endregion
 
   /**Загрузка всех данных об устройствах*/
   loadDevices(): void {
